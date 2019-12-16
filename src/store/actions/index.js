94d75aab4fa1242702aba66b168951e70e3b2138
tao.yang <tao.yang@ihandysoft.com>
2019-12-16 15:53:38 +0800

    编辑器修改

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,39 +1,39 @@
 export function setStep(text) {
-    return {type: 'SET_STEP', text}
+    return { type: "SET_STEP", text };
 }
 export function setWorkTab(text) {
-    return {type: 'SET_WORK_TAB', text}
+    return { type: "SET_WORK_TAB", text };
 }
 export function setWorkSettingTab(text) {
-    return {type: 'SET_WORK_SETTING_TAB', text}
+    return { type: "SET_WORK_SETTING_TAB", text };
 }
 export function setWorkResource(text) {
-    return {type: 'SET_WORK_RESOURCE', text}
+    return { type: "SET_WORK_RESOURCE", text };
 }
 export function setWorkId(text) {
-    return {type: 'SET_WORK_ID', text}
+    return { type: "SET_WORK_ID", text };
 }
 export function setWorkUserId(text) {
-    return {type: 'SET_WORK_USER_ID', text}
+    return { type: "SET_WORK_USER_ID", text };
 }
 export function setConfigId(text) {
-    return {type: 'SET_CONFIG_ID', text}
+    return { type: "SET_CONFIG_ID", text };
 }
 export function setSet1Id(text) {
-    return {type: 'SET_SET1_ID', text}
+    return { type: "SET_SET1_ID", text };
 }
 export function setSet2Id(text) {
-    return {type: 'SET_SET2_ID', text}
+    return { type: "SET_SET2_ID", text };
 }
 export function setWorkGroupId(text) {
-    return {type: 'SET_WORK_GROUP_ID', text}
+    return { type: "SET_WORK_GROUP_ID", text };
 }
 export function setIsFresh(text) {
-    return {type: 'SET_IS_FRESH', text}
+    return { type: "SET_IS_FRESH", text };
 }
 export function setDisabledTab2(text) {
-    return {type: 'SET_DISABLED_TAB2', text}
+    return { type: "SET_DISABLED_TAB2", text };
 }
 export function setWorkType(text) {
-    return {type: 'SET_WORK_TYPE', text}
+    return { type: "SET_WORK_TYPE", text };
 }
